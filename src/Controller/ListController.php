4508a915ff70549868078e8f29fbbb4f39a3c304
yemiwebby <yemiwebby@gmail.com>
2020-03-25 05:40:21 +0100

    Change from GOT to tech companies

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -13,21 +13,21 @@ class ListController extends AbstractController
      */
     public function index(Request $request)
     {
-        $characters = [
-            'Daenerys Targaryen' => 'Emilia Clarke',
-            'Jon Snow'           => 'Kit Harington',
-            'Arya Stark'         => 'Maisie Williams',
-            'Melisandre'         => 'Carice van Houten',
-            'Khal Drogo'         => 'Jason Momoa',
-            'Tyrion Lannister'   => 'Peter Dinklage',
-            'Ramsay Bolton'      => 'Iwan Rheon',
-            'Petyr Baelish'      => 'Aidan Gillen',
-            'Brienne of Tarth'   => 'Gwendoline Christie',
-            'Lord Varys'         => 'Conleth Hill'
+        $companies = [
+            'Apple' => '$1.16 trillion USD',
+            'Samsung' => '$298.68 billion USD',
+            'Microsoft' => '$1.10 trillion USD',
+            'Alphabet' => '$878.48 billion USD',
+            'Intel Corporation' => '$245.82 billion USD',
+            'IBM' => '$120.03 billion USD',
+            'Facebook' => '$552.39 billion USD',
+            'Hon Hai Precision' => '$38.72 billion USD',
+            'Tencent' => '$3.02 trillion USD',
+            'Oracle' => '$180.54 billion USD',
         ];
 
         return $this->render('list/index.html.twig', [
-            'character' => $characters,
+            'companies' => $companies,
         ]);
     }
 }
